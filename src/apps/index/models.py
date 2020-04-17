09aa96dc9424b1f025f0db6d7a65af538d7ed3fa
pharm-me <62742369+pharm-me@users.noreply.github.com>
2020-04-17 22:20:54 +0300

    111 (#1)
    add models

--- src/apps/index/models.py
+++ src/apps/index/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class ArticlePreview(m.Model):
+    article = m.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Article Preview"
+
+    def __str__(self):
+        return "Article Preview"
--- src/apps/index/views.py
+++ src/apps/index/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from apps.index.models import ArticlePreview
+
 
 class IndexView(TemplateView):
     template_name = "index/index.html"
+    def get_context_data(self, **kwargs):
+        parent_ctx = super().get_context_data(**kwargs)
+        info = ArticlePreview.objects.first()
+        ctx = {"object": info}
+        ctx.update(parent_ctx)
+        return ctx
